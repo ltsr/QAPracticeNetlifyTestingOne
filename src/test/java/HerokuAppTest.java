649725b9fdee88584fa3c0ce3e3146ee92b02eb5
ltsr <lukatrifunovict@gmail.com>
2024-11-26 15:30:51 +0100

    Loaded element, Authentication and Slider tests added

--- src/test/java/HerokuAppTest.java
+++ src/test/java/HerokuAppTest.java
@@ -1,6 +1,6 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -15,7 +15,7 @@ public class HerokuAppTest extends DriverInit {
 
     @Test
     public void addRemoveTest() throws InterruptedException {
-        driver.get(baseURL+"add_remove_elements/");
+        driver.get(baseURL + "add_remove_elements/");
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html[1]/body[1]/div[2]/div[1]/div[1]/button[1]")));
 
@@ -34,6 +34,69 @@ public class HerokuAppTest extends DriverInit {
         }
 
 
+    }
 
+    @Test
+    public void loadedElementTest() {
+        driver.get(baseURL + "dynamic_loading");
+
+        driver.findElement(By.xpath("/html/body/div[2]/div/div/a[2]")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("start")));
+
+        driver.findElement(By.xpath("/html/body/div[2]/div/div/div/button")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("finish")));
+
+        String actualText = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[1]/h4")).getText();
+
+        Assert.assertEquals(actualText, "Hello World!");
+    }
+
+    @Test
+    public void basicAuthTest() {
+        driver.get(baseURL + "basic_auth");
+
+        try {
+            ((HasAuthentication) driver).register(UsernameAndPassword.of("admin", "admin"));
+            driver.get("https://the-internet.herokuapp.com/digest_auth");
+
+            // Verify if page is loaded successfully
+            String Title = driver.findElement(By.tagName("h3")).getText();
+            String text = driver.findElement(By.tagName("p")).getText();
+            System.out.println(Title);
+            System.out.println(text);
+
+            Assert.assertEquals(text, "Congratulations! You must have the proper credentials.");
+
+
+        } catch (Exception e) {
+            System.out.println("An unexpected error: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void sliderTest() throws InterruptedException {
+        driver.get(baseURL+"horizontal_slider");
+
+        Thread.sleep(2000);
+
+        int i = 0;
+        for (i = 0; i < 6; i++) {
+            driver.findElement(By.xpath("/html/body/div[2]/div/div/div/input")).sendKeys(Keys.ARROW_RIGHT);
+        }
+        double expectedValueDouble = i * 0.5;
+
+
+        String sliderValueText = driver.findElement(By.xpath("//*[@id=\"range\"]")).getText();
+
+        double sliderValueDouble = Double.parseDouble(sliderValueText);
+
+        Thread.sleep(2000);
+
+        System.out.println("Slider value: " + sliderValueDouble);
+        System.out.printf("Expected value: " + expectedValueDouble);
+
+        Assert.assertEquals(sliderValueDouble, expectedValueDouble);
     }
 }
